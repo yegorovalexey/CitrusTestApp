5a94e0861025d75966968321f1f9bde8c6d8a176
yegorovalexey <egorov@citrus.com.ua>
2014-10-24 17:44:11 +0300

    DeviceRegister Added Google

--- js/clean/PushNotificationClass.js
+++ js/clean/PushNotificationClass.js
@@ -31,22 +31,67 @@ function InitpushNotifications(){
 }
 function successHandler (result) {
 	console.log(result);
-    alert('result = ' + result);
+    //alert('result = ' + result);
 }
 function errorHandler (error) {
 	console.log(result);
-    alert('error = ' + error);
+   // alert('error = ' + error);
 }
 
 // handle GCM notifications for Android
+/*
+e = Object {regid: "APA91bF_xZ1w0WkxQ-N-Patuw88volk0606XZxQlloS3pEdYEz…KLBo5Mfe89g4SchRs9EPmwRNFpWMQnOAKDuI5po0nRbjCYpbU", event: "registered"}
+event: "registered"
+regid: "APA91bF_xZ1w0WkxQ-N-Patuw88volk0606XZxQlloS3pEdYEznLG5foQCSqJu09DCgNmtyfhmd1eA9XzU3Snpghp93ct0h_GkCYfu1ZFP2gRDeugNpCG8_K0xZ4hv2cBd4KptKLBo5Mfe89g4SchRs9EPmwRNFpWMQnOAKDuI5po0nRbjCYpbU"
+}
+ */
+ 
 function onNotification(e) {
 	console.log(e);
-    alert('Notification = ' + e.event);
+	RegisterDevice(e.event.regid,"google");
+   // alert('Notification = ' + e.event);
 }
 
 // handle APNS notifications for iOS
 
 function onNotificationAPN(e) {
 	console.log(e);
-    alert('Notification = ' + e.alert);
+   // alert('Notification = ' + e.alert);
+}
+
+
+function AfterRegisterDevice(data){
+    console.log(data);
+  //  alert(data);
+}
+
+function RegisterDevice(key,provider,mobile){
+    var phone = "";
+    if(arguments.length==3 && mobile!= undefined){
+        phone = mobile;
+    }
+    var php_path = "device.php";
+    var data = 'register&key='+key+'&mobile='+phone+'&provider='+provider;
+
+
+
+    $.ajax({
+        url: "http://m.citrus.ua/ajax/on/"+php_path+"?method="+data,
+        dataType: 'json',
+        async: true,
+        success: function( json ) {
+
+            AfterRegisterDevice(json);
+        },
+        timeout: 8000 ,
+        error: function(jqXHR, status, errorThrown){   //the status returned will be "timeout"
+            if(status == "timeout"){
+                ShowMessage(1);
+                return false;
+
+            }
+
+        }
+    });
+
 }
